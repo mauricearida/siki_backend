dfef0e4eeffc963f42c653fe1c69110112628d1f
maurice <maurice.arida66@gmail.com>
2023-02-02 15:27:16 +0200

    fixed nodemonaaa

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -4,9 +4,8 @@ const {
   getOneRestaurantById,
 } = require("../services/restaurant.service");
 var router = express.Router();
-console.log("343434343443");
+
 router.get("/", async (req, res) => {
-  console.log("1212121212");
   let response = await getAllRestaurant();
   res.json(response);
 });
